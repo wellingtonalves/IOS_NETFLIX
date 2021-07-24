3f6fc4be547814a7ca97026ab34e1015e5516a26
Wellington Alves Gomes <wellington.alves.gomes@gmail.com>
2021-07-23 21:30:17 -0300

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Wellington Alves on 23/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
